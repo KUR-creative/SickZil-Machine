e1a0e3b8258ac0b51c02af258614ae2bc9c4d56c
kur <rhdnfka94@gmail.com>
2019-07-04 16:49:00 +0900

    Add config.py

---
+++ src/config.py
@@ -0,0 +1,2 @@
+main_qml = '../qml/szmc-0.1.0.qml'
+main_qml_context = 'main'
--- src/gui.py
+++ src/gui.py
@@ -1,11 +1,13 @@
+import config
 from PyQt5.QtCore import QObject
 
 class MainWindow(QObject):
     def __init__(self,engine):
         QObject.__init__(self)
 
-        engine.rootContext() \
-              .setContextProperty('main', self)
-        engine.load('../qml/szmc-0.1.0.qml')
+        engine.rootContext().setContextProperty(
+            config.main_qml_context, self
+        )
+        engine.load(config.main_qml)
 
         self.window = engine.rootObjects()[0]

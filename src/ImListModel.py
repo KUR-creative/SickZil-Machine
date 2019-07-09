1fea92adf11218ae5842cf7e2a3fdb31510f74db
kur <rhdnfka94@gmail.com>
2019-07-09 14:02:35 +0900

    Tidy up ImListModel code

--- src/ImListModel.py
+++ src/ImListModel.py
@@ -5,17 +5,10 @@ import state
 
 
 class ImListModel(QAbstractListModel):
-    imagePath= Qt.UserRole + 1
-    maskPath = Qt.UserRole + 2
-    displayed= Qt.UserRole + 3
-
     def __init__(self, parent=None):
         super().__init__(parent)
         self.images = ()
         self.masks = ()
-
-    #def open(self, img_paths, mask_paths):
-    #    self.update(img_paths, mask_paths)
 
     def update(self, img_paths=None, mask_paths=None):
         if (img_paths is None and mask_paths is None):
@@ -30,19 +23,20 @@ class ImListModel(QAbstractListModel):
         def content(path):
             p = Path(path)
             return str(Path( p.parent.name, p.name ))
-        self.images= fp.lmap(content, img_paths)
-        self.masks = fp.lmap(content, mask_paths)
+        self.images= fp.lmap(content, img_paths)  # SET STATE!
+        self.masks = fp.lmap(content, mask_paths) # SET STATE!
         self.endInsertRows() #-----------------------
 
+    imagePath = Qt.UserRole + 1
+    maskPath  = Qt.UserRole + 2
+    displayed = Qt.UserRole + 3
     def data(self, index, role=Qt.DisplayRole):
-        #print('data called! index =', index, 'role =', role)
         y = index.row()
-        if role == ImListModel.imagePath:
-            return self.images[y]
-        if role == ImListModel.maskPath:
-            return self.masks[y]
-        if role == ImListModel.displayed: 
-            return y == state._cursor
+        return {
+            self.imagePath: self.images[y],
+            self.maskPath : self.masks[y],
+            self.displayed: y == state._cursor 
+        }[role]
 
     def rowCount(self, parent=QModelIndex()):
         return len(self.images)
@@ -50,7 +44,7 @@ class ImListModel(QAbstractListModel):
     def roleNames(self): 
         ''' specify role names in qml '''
         return {
-            ImListModel.imagePath: b'image',
-            ImListModel.maskPath: b'mask',
-            ImListModel.displayed: b'displayed'
+            self.imagePath: b'image',
+            self.maskPath : b'mask',
+            self.displayed: b'displayed'
         }

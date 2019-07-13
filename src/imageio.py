8f488cec13eebb4be0847b7288735ce39fa17614
kur <rhdnfka94@gmail.com>
2019-07-13 22:42:20 +0900

    Load QImage directly instead of nparr->qimg (gui tested)

--- src/imageio.py
+++ src/imageio.py
@@ -1,16 +1,24 @@
 import utils.imutils as iu #TODO: make imutils minimal.
 import utils.fp as fp
+import cv2
+from PyQt5.QtGui import QImage
 
 def load(path):
     # it can be cached..
     return iu.imread(path)
 
+def save(img, path): #TODO: multimethod..?
+    cv2.imwrite(img, path)
+
 def load_qimg(path):
+    return QImage(path)
+    '''
     return fp.go(
         path,
         load,
         iu.channel3img,
         iu.nparr2qimg
     )
+    '''
 
 #def load_mask(path)

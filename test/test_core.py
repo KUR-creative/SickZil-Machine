cf3d850cd537496f33a1ad24e89ac36c1941ab02
kur <rhdnfka94@gmail.com>
2019-07-13 18:34:21 +0900

    Add multiple image look&feel test

--- test/test_core.py
+++ test/test_core.py
@@ -5,13 +5,14 @@ import core
 import cv2
 
 def test_segment_input_output_spec_check():
-    img = cv2.imread('../test/fixture/real_imgs/bw1.png')
-    ret = core.segmap(img)
+    img1= cv2.imread('../test/fixture/real_imgs/bw1.png')
+    img2= cv2.imread('../test/fixture/real_imgs/bgr1.png')
+    ret1= core.segmap(img1)
+    ret2= core.segmap(img2)
 
     # look & feel
-    cv2.imshow('img',img)
-    cv2.imshow('ret',ret)
+    cv2.imshow('img1',img1); cv2.imshow('img2',img2);
+    cv2.imshow('ret1',ret1); cv2.imshow('ret2',ret2);
     cv2.waitKey(0)
 
 # test different data(robustness)
-# test in multiple images

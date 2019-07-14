7de33e508167afcb30206d937a0f3a2aef2c7ee5
kur <rhdnfka94@gmail.com>
2019-07-14 22:27:47 +0900

    Reorder functions and refactor next/prev using cursor

--- src/state.py
+++ src/state.py
@@ -15,43 +15,7 @@ img_paths = ()
 mask_paths= ()
 _cursor = 0 # NOTE: private! DO NOT ACCESS!!!!
 
-def cursor(new=None):
-    global _cursor
-    if new is None: # getter
-        return _cursor
-    elif img_paths: # setter
-        _cursor = new % len(img_paths)
-
-def next():
-    global _cursor
-    if img_paths:
-        _cursor = (_cursor + 1) % len(img_paths)
-def prev():
-    global _cursor
-    if img_paths:
-        _cursor = (_cursor - 1) % len(img_paths)
-
-def project():
-    global img_paths, mask_paths, _cursor
-    return namedtuple(
-        'Project', 
-        'img_paths mask_paths')(
-         img_paths,mask_paths
-    )
-
-def dir_type(dirpath):
-    parent = Path(dirpath)
-
-    prjdir = ((parent / config.IMGDIR).exists()
-          and (parent / config.MASKDIR).exists())
-    imgdir = any(fp.map(
-        iu.is_img_file, fu.children(parent)
-    ))
-
-    return(config.PRJDIR      if prjdir 
-      else config.FLAT_IMGDIR if imgdir 
-      else config.UNSUPPORT_DIR)
-
+#-----------------------------------------------
 def now_image():
     global img_paths, _cursor
     if img_paths:
@@ -62,6 +26,15 @@ def now_mask():
     if mask_paths:
         return mask_paths[_cursor]
 
+def project():
+    global img_paths, mask_paths, _cursor
+    return namedtuple(
+        'Project', 
+        'img_paths mask_paths')(
+         img_paths,mask_paths
+    )
+
+#-----------------------------------------------
 def set_project(prj_dirpath):
     assert Path(prj_dirpath,config.IMGDIR).exists()
     assert Path(prj_dirpath,config.MASKDIR).exists()
@@ -84,3 +57,31 @@ def clear_all():
     img_paths = ()
     mask_paths = ()
     _cursor = 0
+
+def cursor(new=None):
+    global _cursor
+    if new is None: # getter
+        return _cursor
+    elif img_paths: # setter
+        _cursor = new % len(img_paths)
+
+def next():
+    global _cursor
+    cursor(_cursor + 1)
+def prev():
+    global _cursor
+    cursor(_cursor - 1)
+
+#-----------------------------------------------
+def dir_type(dirpath):
+    parent = Path(dirpath)
+
+    prjdir = ((parent / config.IMGDIR).exists()
+          and (parent / config.MASKDIR).exists())
+    imgdir = any(fp.map(
+        iu.is_img_file, fu.children(parent)
+    ))
+
+    return(config.PRJDIR      if prjdir 
+      else config.FLAT_IMGDIR if imgdir 
+      else config.UNSUPPORT_DIR)

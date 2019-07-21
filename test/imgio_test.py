4afb3cce430607ba6973529da184f4b2b1cb9849
kur <rhdnfka94@gmail.com>
2019-07-21 21:55:54 +0900

    Rename masks/bgr1.png to other fixture folder

--- test/imgio_test.py
+++ test/imgio_test.py
@@ -7,14 +7,14 @@ import numpy as np
 import imgio as io
 
 def test_load():
-    path = './fixture/real_proj/masks/bgr1.png'
+    path = './fixture/not_proj_dir/bgr1_mask.png'
     expected_qimg = QImage(path)
     expected_ndarr= cv2.imread(path, cv2.IMREAD_UNCHANGED)
     assert io.load(path) == expected_qimg
     assert np.array_equal(io.load(path,io.NDARR),expected_ndarr)
 
 def test_load_image():
-    for path in ['./fixture/real_proj/masks/bgr1.png',
+    for path in ['./fixture/not_proj_dir/bgr1_mask.png',
                  './fixture/real_proj/images/bgr1.png',
                  './fixture/real_proj/images/bw1.png']:
         actual = io.load(path, io.IMAGE)
@@ -22,7 +22,7 @@ def test_load_image():
         assert np.array_equal(actual,expected)
 
 def test_load_mask():
-    path = './fixture/real_proj/masks/bgr1.png'
+    path = './fixture/not_proj_dir/bgr1_mask.png'
     h,w = cv2.imread(path, cv2.IMREAD_UNCHANGED).shape[:2]
     b,g,r = cv2.split(io.load(path,io.MASK))
     assert h == b.shape[0]
